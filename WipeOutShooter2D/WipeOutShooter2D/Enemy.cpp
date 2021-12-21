0ddf78ae077212817af2eb017943ae9c34b5698d
MrGarrison7212 <bokipfc1992@gmail.com>
2021-12-21 23:19:15 +0100

    Adding Enemy functions.

--- WipeOutShooter2D/WipeOutShooter2D/Enemy.cpp
+++ WipeOutShooter2D/WipeOutShooter2D/Enemy.cpp
@@ -4,9 +4,35 @@
 
 Enemy::Enemy()
 {
+	this->shape.setFillColor(sf::Color::Magenta);
+	this->shape.setSize(sf::Vector2f(50.f, 50.f));
 }
 
 
 Enemy::~Enemy()
 {
+}
+
+const sf::Vector2f & Enemy::getPos() const
+{
+	return this->shape.getPosition();
+}
+
+const sf::FloatRect Enemy::getBounds() const
+{
+	return this->shape.getGlobalBounds();
+}
+
+void Enemy::setPos(sf::Vector2f vect)
+{
+	this->shape.setPosition(vect);
+}
+
+void Enemy::update()
+{
+}
+
+void Enemy::render(sf::RenderTarget * target)
+{
+	target->draw(this->shape);
 }
--- WipeOutShooter2D/WipeOutShooter2D/Enemy.h
+++ WipeOutShooter2D/WipeOutShooter2D/Enemy.h
@@ -1,8 +1,23 @@
 #pragma once
+#include <SFML/Graphics.hpp>
+
+
 class Enemy
 {
+
+private:
+	sf::RectangleShape shape;
+
+
 public:
 	Enemy();
 	virtual ~Enemy();
+	//functions
+	const sf::Vector2f& getPos() const;
+	const sf::FloatRect getBounds() const;
+
+	void setPos(sf::Vector2f vect);
+	void update();
+	void render(sf::RenderTarget* target);
 };
 
